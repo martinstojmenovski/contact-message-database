bd6ccd4433c8e36836c7317607f4a49f0bed9233
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2023-04-18 15:09:55 -0600

    uploading database

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect("mongodb+srv://martinstojmenovskim:martin1995@cluster-collect-message.x88rlcf.mongodb.net/?retryWrites=true&w=majority")
 const db = mongoose.connection
 
 db.on('error', (error) => console.error(error))
