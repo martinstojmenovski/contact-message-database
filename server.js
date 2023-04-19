f833f20ab88dd741811345787745ccc7567d01a9
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2023-04-19 13:46:30 -0600

    make it better

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
 const db = mongoose.connection
 
 db.on('error', (error) => console.error(error))
